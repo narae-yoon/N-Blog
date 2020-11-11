a96b1699e2eecad486f3b47f6d5bd3300e73881a
narae-yoon <nare6557@gmail.com>
2020-11-12 01:07:02 +0900

    회원가입 로직 완성

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.use(bodyParser.urlencoded({ extended:false }));
 
 
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', {state: 1});
 });
 
 // 로그인
@@ -30,7 +30,7 @@ router.post('/login', (req, res) => {
         conn.query(sql, (err, result) => {
             if(err) {
                 console.log('[로그인: 실패] db 오류');
-                res.redirect('/users/login');
+                res.render('login', {state: 2});
             }
             if (result[0].cnt) {
                 sql = mybatisMapper.getStatement('user', 'selectLogin', param, format);
@@ -39,17 +39,17 @@ router.post('/login', (req, res) => {
                         console.log('[로그인: 실패] 비밀번호 오류');
                         sql = mybatisMapper.getStatement('user','addPwErr', param, format);
                         conn.query(sql);
-                        res.redirect('/users/login');
+                        res.render('login',{state: 2});
                     }else {
                         console.log('[로그인: 성공]')
                         sql = mybatisMapper.getStatement('user','resetPwErr', param, format);
                         conn.query(sql);
-                        //res.redirect('/');
+                        res.redirect('/');
                     }
                 })
             } else {
                 console.log('[로그인: 실패] 등록되지 않은 이메일');
-                res.redirect('/users/login');
+                res.render('login',{state: 2});
             }
         })
     });
@@ -62,11 +62,22 @@ router.get('/signUp', (req, res) => {
 router.post('/signUp', (req, res) => {
     const param = {
         'email': req.body.email,
-        'password': req.body.password,
+        'pw': req.body.pw,
         'nickname': req.body.nickname,
         'gender': req.body.gender,
-        'introduce': req.body.introduce
+        'intro': req.body.intro
     };
+    const sql = mybatisMapper.getStatement('user','insertUser', param, format);
+
+    pool.getConnection((err, conn) => {
+        conn.query(sql, (err, result) => {
+            if (err) {
+                console.log('[회원가입: 실패] db 오류');
+            }
+            console.log('[회원가입: 성공!]');
+            res.render('login',{state: 0});
+        })
+    })
 });
 
 module.exports = router;
