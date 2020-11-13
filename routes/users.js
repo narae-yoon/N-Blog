877b95c89186bc9b4e0adcfff9a67475437806e7
narae-yoon <nare6557@gmail.com>
2020-11-14 04:32:05 +0900

    비밀번호 암호화

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mybatisMapper = require('mybatis-mapper');
 const pool = require('../config/database');
+const bcrypt = require('bcryptjs');
 
 const format = {language:'sql',indent:' '};
+const saltRounds = 10;
 const router = express.Router();
 
 mybatisMapper.createMapper(['./public/mapper/userMapper.xml']);
@@ -11,6 +13,26 @@ mybatisMapper.createMapper(['./public/mapper/userMapper.xml']);
 router.use(express.static('public'));
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended:false }));
+
+// 비밀번호 암호화
+function hashingPw (pw){
+    return new Promise ((resolve, reject) => {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if(err) {
+                reject(err);
+            }else {
+                 bcrypt.hash(pw, salt, (err, hash) => {
+                    if(err) {
+                        reject(err);
+                    }else {
+                        resolve(hash);
+                    }
+                });
+            }
+        });
+    });
+}
+
 
 
 router.get('/login', (req, res) => {
@@ -37,28 +59,34 @@ router.post('/login', (req, res) => {
             if (result[0].cnt) {
                 sql = mybatisMapper.getStatement('user', 'selectLogin', param, format);
                 conn.query(sql, (err, userInfo) => {
-                    if (userInfo[0].MBR_PW != user.pw) {
-                        console.log('[로그인: 실패] 비밀번호 오류');
-                        sql = mybatisMapper.getStatement('user','addPwErr', param, format);
-                        conn.query(sql);
-                        res.render('login',{state: 2});
-                    }else {
-                        console.log('[로그인: 성공]')
-                        sql = mybatisMapper.getStatement('user','resetPwErr', param, format);
-                        conn.query(sql);
-
-                        req.session.login = {
-                            no: userInfo[0].MBR_NO,
-                            email: userInfo[0].MBR_EMAIL,
-                            nickname: userInfo[0].MBR_NKNM,
-                            intro: userInfo[0].MBR_INTRO
-                        };
-
-                        req.session.save(() => {
-                            res.render('index', {login: true});
-                        });
-                    }
-                })
+                    bcrypt.compare(user.pw, userInfo[0].MBR_PW, (err, success) => {
+                        if(err) {
+                            console.log('bcrypt.compare() error : ', err.message);
+                        }else {
+                            if(!success) {
+                                console.log('[로그인: 실패] 비밀번호 오류');
+                                sql = mybatisMapper.getStatement('user','addPwErr', param, format);
+                                conn.query(sql);
+                                res.render('login',{state: 2});
+                            }else {
+                                console.log('[로그인: 성공]')
+                                sql = mybatisMapper.getStatement('user','resetPwErr', param, format);
+                                conn.query(sql);
+        
+                                req.session.login = {
+                                    no: userInfo[0].MBR_NO,
+                                    email: userInfo[0].MBR_EMAIL,
+                                    nickname: userInfo[0].MBR_NKNM,
+                                    intro: userInfo[0].MBR_INTRO
+                                };
+        
+                                req.session.save(() => {
+                                    res.render('index', {login: true});
+                                });
+                            }
+                        }
+                    });
+                });
             } else {
                 console.log('[로그인: 실패] 등록되지 않은 이메일');
                 res.render('login',{state: 2});
@@ -72,24 +100,37 @@ router.get('/signUp', (req, res) => {
 });
 
 router.post('/signUp', (req, res) => {
-    const param = {
-        'email': req.body.email,
-        'pw': req.body.pw,
-        'nickname': req.body.nickname,
-        'gender': req.body.gender,
-        'intro': req.body.intro
-    };
-    const sql = mybatisMapper.getStatement('user','insertUser', param, format);
-
+    let param = { 'email': req.body.email };
+    const sql = mybatisMapper.getStatement('user','selectEmailCnt', param, format);
     pool.getConnection((err, conn) => {
         conn.query(sql, (err, result) => {
-            if (err) {
-                console.log('[회원가입: 실패] db 오류');
+            if(result.length > 0) {
+                console.log('등록된 사용자입니다.');
+            }else {
+                hashingPw(req.body.pw)
+                    .then((hashedPw) => {
+                        param = {
+                            'email': req.body.email,
+                            'pw': hashedPw,
+                            'nickname': req.body.nickname,
+                            'gender': req.body.gender,
+                            'intro': req.body.intro
+                        };
+                        const sql2 = mybatisMapper.getStatement('user','insertUser', param, format);
+                        pool.getConnection((err, conn) => {
+                            conn.query(sql2, (err, result) => {
+                                if (err) {
+                                    console.log('[회원가입: 실패] db 오류');
+                                }
+                                console.log('[회원가입: 성공!]');
+                                res.render('login',{state: 0});
+                            })
+                        });
+                    })
+                    .catch((err) => { console.log(err) })
             }
-            console.log('[회원가입: 성공!]');
-            res.render('login',{state: 0});
-        })
-    })
+        });
+    });
 });
 
 module.exports = router;
